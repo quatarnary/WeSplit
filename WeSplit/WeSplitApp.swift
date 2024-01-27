e54e4783597f30c9c49b20f9ed9b8edfa19a3743
quatarnary <contact@ilyasbugra.com>
2024-01-27 17:20:13 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Bugra Aslan on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
